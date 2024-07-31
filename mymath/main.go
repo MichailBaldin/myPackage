da2736de2c87f7ce20fffd0f14ecb268e55321aa
Mick <michailbaldin@gmail.com>
2024-07-31 22:50:58 +0300

    add Abs and Yn

--- mymath/main.go
+++ mymath/main.go
@@ -27,3 +27,11 @@ func Min(x, y float64) float64 {
 
 	return math.Min(x, y)
 }
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
